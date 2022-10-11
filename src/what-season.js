421afbc0f14530bbcaf22e9744971c9bcf36da06
Kris <Krismail>
2022-10-11 13:52:48 +0300

    faet: add what-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  console.log(date)
+  if(!date){
+    return 'Unable to determine the time of year!'
+  }
+  // if(Object.getOwnPropertyNames(date) === [] || date === 'undefined'){
+  //   throw new Error("Invalid date!")
+  // }
+    try{
+      console.log(typeof(date),date.getMonth());
+        let m = date.getMonth();
+        
+        switch (m) {
+        case 0:return 'winter';
+        case 1:return 'winter';
+        case 2:return 'spring';
+        case 3:return 'spring';
+        case 4:return 'spring';
+        case 5:return 'summer';
+        case 6:return 'summer';
+        case 7:return 'summer'; 
+        case 8:return 'autumn';
+        case 9:return 'autumn';
+        case 10:return 'autumn';   
+        case 11:return 'winter';
+      }
+    }catch(err){
+      throw new Error("Invalid date!")
+    }
+  // }else{
+  //   throw new Error("Invalid date!")
+  // }
+
 }
 
 module.exports = {
